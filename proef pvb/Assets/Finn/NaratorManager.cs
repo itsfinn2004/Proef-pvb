92d67a49c3b8ef3111188bbaa44f332223d7df99
Finn <fstreunding@gmail.com>
2025-04-22 12:56:46 +0200

    narator manager is klaar
    narator manager is klaar, alleen de voicelines zelf moeten er nog in

--- proef pvb/Assets/Finn/NaratorManager.cs
+++ proef pvb/Assets/Finn/NaratorManager.cs
@@ -1,8 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class NaratorManager : MonoBehaviour
 {
+    public AudioClip[] VoiceLines;
+    public AudioSource audiosource;
     
+
+    private void Start()
+    {
+        audiosource = GameObject.Find("audioManager").GetComponent<AudioSource>();
+
+    }
+
+    public void PlayAudio(int number)
+    {
+        audiosource.clip = VoiceLines[number];
+        audiosource.Play();
+    }
 }
---
+++ proef pvb/Assets/Finn/NaratorTrigger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class NaratorTrigger : MonoBehaviour
+{
+   public NaratorManager nm;
+    public int NumberToPlay;
+    private void Awake()
+    {
+        nm = GameObject.Find("audioManager").GetComponent<NaratorManager>(); 
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("player colided");
+            nm.PlayAudio(NumberToPlay);
+        }
+    }
+}
